7d25932c51d7dd7d65dc0940fbcf20484b720f50
solutionsSlayer <ddoussain@gmail>
2019-05-14 14:27:43 +0200

    Add toolbar with some colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatToolbarModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AppComponent
   ],
   imports: [
+    MatToolbarModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
     MatIconModule,
