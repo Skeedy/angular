88f37feb3956c5516685f772bd8d03764f363e7e
solutionsSlayer <ddoussain@gmail>
2019-05-14 13:17:36 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatIconModule,
     BrowserModule
   ],
   providers: [],
